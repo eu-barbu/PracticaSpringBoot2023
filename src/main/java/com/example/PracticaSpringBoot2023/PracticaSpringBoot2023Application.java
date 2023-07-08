e238faea5cf0116f3e6111ae03db30017f7b4aa2
eu-barbu <eu.barbu@gmail.com>
2023-07-08 14:30:58 +0300

    Training 1: Adding a new controller and a HTML page

---
+++ src/main/java/com/example/PracticaSpringBoot2023/PracticaSpringBoot2023Application.java
@@ -0,0 +1,13 @@
+package com.example.PracticaSpringBoot2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSpringBoot2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSpringBoot2023Application.class, args);
+	}
+
+}
